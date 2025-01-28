87885cdad796973f2c1862a5a46226fc38e332f3
GoodLua <doanhuuduy24@gmail.com>
2025-01-28 19:02:55 +0700

    Add files via upload

--- Script.lua
+++ Script.lua
@@ -339,16 +339,14 @@ spawn(function()
  game:GetService("RunService").RenderStepped:Wait()
  end
  end)
- 
---[[
-local Logo = Instance.new("ImageLabel")
+ local Logo = Instance.new("ImageLabel")
 	Logo.Name = "Logo"
 	Logo.Parent = Main
 	Logo.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 	Logo.BackgroundTransparency = 1
 	Logo.Position = UDim2.new(0, 10, 0, 10)
 	Logo.Size = UDim2.new(0, 120, 0, 120)
-	Logo.Image = "rbxassetid://13797406183"]]--
+	Logo.Image = "rbxassetid://91556276394723"
 local Tab = Instance.new("Frame")
 Tab.Name = "Tab"
 Tab.Parent = Main
@@ -17216,7 +17214,7 @@ ImageButton1.BorderSizePixel = 0
 ImageButton1.Position = UDim2.new(0.120833337, 0, 0.0952890813, 0)
 ImageButton1.Size = UDim2.new(0, 50, 0, 50)
 ImageButton1.Draggable = true
-ImageButton1.Image = "http://www.roblox.com/asset/?id=91556276394723"
+ImageButton1.Image = "rbxassetid://91556276394723"
 ImageButton1.MouseButton1Down:connect(function()
   game:GetService("VirtualInputManager"):SendKeyEvent(true,305,false,game)
   game:GetService("VirtualInputManager"):SendKeyEvent(false,305,false,game)
