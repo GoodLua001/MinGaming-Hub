9adefb11c37525e69115495b17551953118d1206
GoodLua <doanhuuduy24@gmail.com>
2025-01-28 18:51:44 +0700

    Add files via upload

--- Script.lua
+++ Script.lua
@@ -2291,30 +2291,7 @@ return main
 end
 return uitab
 end
-local ScreenGui1 = Instance.new("ScreenGui")
-local ImageButton1 = Instance.new("ImageButton")
-local UICorner = Instance.new("UICorner")
-local UIStroke = Instance.new("UIStroke")
 
-ScreenGui1.Name = "ImageButton"
-ScreenGui1.Parent = game.CoreGui
-ScreenGui1.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-
-ImageButton1.Parent = ScreenGui1
-ImageButton1.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-ImageButton1.BorderSizePixel = 0
-ImageButton1.Position = UDim2.new(0.120833337, 0, 0.0952890813, 0)
-ImageButton1.Size = UDim2.new(0, 50, 0, 50)
-ImageButton1.Draggable = true
-ImageButton1.Image = "http://www.roblox.com/asset/?id=91556276394723"
-ImageButton1.MouseButton1Down:connect(function()
-  game:GetService("VirtualInputManager"):SendKeyEvent(true,305,false,game)
-  game:GetService("VirtualInputManager"):SendKeyEvent(false,305,false,game)
-end)
-UICorner.Parent = ImageButton1
-UIStroke.Color = Color3.fromRGB(252, 3, 161)
-UIStroke.Thickness = 1.5
-UIStroke.Parent = ImageButton1
 --------------------------------------------------------------------------------------------------------
 if game.PlaceId == 2753915549 then
         World1 = true
@@ -17223,7 +17200,31 @@ spawn(function()
     end)
     end)
 FakeDamage("Information")
+---------------------------------------------------------------------------------------------------------------------------------------
+local ScreenGui1 = Instance.new("ScreenGui")
+local ImageButton1 = Instance.new("ImageButton")
+local UICorner = Instance.new("UICorner")
+local UIStroke = Instance.new("UIStroke")
 
+ScreenGui1.Name = "ImageButton"
+ScreenGui1.Parent = game.CoreGui
+ScreenGui1.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+ImageButton1.Parent = ScreenGui1
+ImageButton1.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+ImageButton1.BorderSizePixel = 0
+ImageButton1.Position = UDim2.new(0.120833337, 0, 0.0952890813, 0)
+ImageButton1.Size = UDim2.new(0, 50, 0, 50)
+ImageButton1.Draggable = true
+ImageButton1.Image = "http://www.roblox.com/asset/?id=91556276394723"
+ImageButton1.MouseButton1Down:connect(function()
+  game:GetService("VirtualInputManager"):SendKeyEvent(true,305,false,game)
+  game:GetService("VirtualInputManager"):SendKeyEvent(false,305,false,game)
+end)
+UICorner.Parent = ImageButton1
+UIStroke.Color = Color3.fromRGB(252, 3, 161)
+UIStroke.Thickness = 1.5
+UIStroke.Parent = ImageButton1
 ---------------------------------------------------------------------------------------------------------------------------------------
 local player = game.Players.LocalPlayer
 
